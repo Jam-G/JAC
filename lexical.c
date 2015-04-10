98d9ec5702c1ed7bb68922fa158472285bfb7743
Jam-G <cruijunguo@gmail.com>
2015-04-10 20:17:42 +0800

    lexical

--- lexical.c
+++ lexical.c
@@ -1,19 +1,19 @@
-#include"lexcal.h"
+#include"lexical.h"
 #include<stdlib.h>
 #include<sys/types.h>
 #include<stdio.h>
 #include<malloc.h>
 #include<stdarg.h>
-struct Node *createNode(int row, int tp, int tt, char *nm){
+struct Node *createNode(int row, enum Type tp, enum TokenType tt, char *nm){
 	struct Node * nd = (struct Node *)malloc(sizeof(struct Node));
 	nd ->lineno = row;
 	nd ->type = tp;
 	nd ->tokentype = tt;
-	nd -name = (char *)malloc(strlen(nm) + 1);
+	nd ->name = (char *)malloc(strlen(nm) + 1);
 	strcpy(nd ->name, nm);
 	nd ->name[strlen(nm)] = '\0';//add the end of string in case strcpy did not add
 	nd ->brother = NULL;
-	bd ->child = NULL;
+	nd ->child = NULL;
 	return nd;
 }
 
@@ -29,7 +29,7 @@ struct Node *destroyNode(struct Node *gb){
 }
 
 struct Node *addChild(struct Node *ft, int cn, ...){
-	if(ft == NULL || cd == NULL || cn <= 0){
+	if(ft == NULL || cn <= 0){
 		return NULL;
 	}
 	va_list ap;
@@ -43,7 +43,9 @@ struct Node *addChild(struct Node *ft, int cn, ...){
 	for(; addpoint ->brother != NULL; addpoint = addpoint ->brother);
 	for(;cn > 0; cn --){
 		addpoint ->brother = va_arg(ap, struct Node*);
-		addpoion = addpoint ->brother;
+		addpoint = addpoint ->brother;
+		if(addpoint == NULL)
+			break;
 	}
 	return addpoint;
 }
@@ -51,5 +53,5 @@ struct Node *addChild(struct Node *ft, int cn, ...){
 struct Node *VLRprintTree(struct Node *rt){
 	if(rt == NULL)
 		return NULL;
-	
+	return NULL;
 }
--- lexical.h
+++ lexical.h
@@ -1,16 +1,20 @@
 #ifndef LEXICAL_H
 #define LEXICAL_H
 #include<stdarg.h>
+enum Type{END, NOTEND};
+enum TokenType{
+	INT, FLOAT, ID, SEMI, COMMA, ASSIGNOP, RELOP, PLUS, MINUS, STAR, DIV, AND, OR, DOT, NOT, TYPE, LP, RP, LB, RB, LC, RC, STRUCT, RETURN, IF, ELSE, WHILE
+};
 struct Node{
 	int lineno;
-	int type;
-	int tokentype;
+	enum Type type;
+	enum TokenType tokentype;
 	char *name;
 	struct Node *brother;//左兄弟,右孩子结构的树
 	struct Node *child;
 };
 
-struct Node *createNode(int row, int tp, int tt, char *nm);
+struct Node *createNode(int row, enum Type tp, enum TokenType tt, char *nm);
 struct Node *destroyNode(struct Node *gb);
 struct Node *addChild(struct Node *ft, int cn, ...);
 struct Node *printTree(struct Node *rt, int nLayer);
