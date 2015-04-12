63e6e2b08e06263eafee73bb77d7e6843c8b216c
Jam-G <cruijunguo@gmail.com>
2015-04-12 14:20:28 +0800

    finish make testall

--- Makefile
+++ Makefile
@@ -9,12 +9,23 @@ bison:syntax-analyzer.y
 	bison -d -t syntax-analyzer.y
 parser:main.c syntax-analyzer.tab.c syntax-analyzer.tab.h lexical.c lexical.h lex.yy.c
 	gcc main.c syntax-analyzer.tab.c lexical.c -Wall -lfl -ly -o parser
-.PHONY:clean debug
+.PHONY:clean debug test testall
 clean :
 	rm -f lex.yy.c
 	rm -f syntax-analyzer.tab.*
 	rm -f parser
 	rm -f *.output	
 debug:flex bisondebug parser
-
-
+test:
+	./parser test1.cmm
+	./parser test2.cmm
+	./parser test3.cmm
+	./parser test4.cmm
+	./parser mtest1.cmm
+	./parser mtest2.cmm
+	./parser mtest3.cmm
+	./parser mtest4.cmm
+	./parser mtest5.cmm
+	./parser mtest6.cmm
+testall:
+	./parser *.cmm	
--- main.c
+++ main.c
@@ -7,20 +7,25 @@ int haserror;
 int main(int argc, char **argv){
 	if(argc <= 1)
 		return -1;
-	FILE *f = fopen(argv[1], "r");
-	if(!f){
-		perror("file not found\n" );
-		return -1;
+	int i = 1;
+	for(i = 1; i < argc; i ++){
+		printf("\n\ntesting:%s\n", argv[i]);
+		FILE *f = fopen(argv[i], "r");
+		if(!f){
+			perror("file not found\n" );
+			return -1;
+		}
+		root = NULL;
+		yylineno = 1;
+		haserror = 0;
+		yyrestart(f);
+		yyparse();
+		if(haserror == 0){
+			printTree(root, 0);
+		}
+		destroyTree(root);
+		close(f);
 	}
-	root = NULL;
-	yylineno = 1;
-	haserror = 0;
-	yyrestart(f);
-	yyparse();
-	if(haserror == 0){
-		printTree(root, 0);
-	}
-	destroyTree(root);
 	return 0;
 }
 
