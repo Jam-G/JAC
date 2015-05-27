79188c7c8ac73a66a332edd8eded22969a993c98
Jam-G <cruijunguo@gmail.com>
2015-05-27 22:08:53 +0800

    delete tree

--- main.c
+++ main.c
@@ -82,7 +82,7 @@ int main(int argc, char **argv){
 		yyrestart(f);
 		yyparse();
 		if(haserror == 0){
-			printTree(root, 0);
+		//	printTree(root, 0);
 			semantic();
 			traverse(&gen_ir, root, 0);
 			optimize_ir(&irtable);
